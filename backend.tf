fd669825be072db858207ee9fa5739657732875e
Navjit-Kaur-TX <132881216+Navjit-Kaur-TX@users.noreply.github.com>
2024-06-17 17:16:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,16 @@
 access_key = ${{ secrets.ACCESS_KEY }}
 secret_key = ${{ secrets.SECRET }}
-bucket = "aws-tf-state-bucket-tx"  
-key    = "remotedemo.tfstate"  
-region = "us-east-1"
+#bucket = "aws-tf-state-bucket-tx"  
+#key    = "remotedemo.tfstate"  
+#region = "us-east-1"
+terraform {
+    backend "remote" {
+        hostname = "gurpreet1.jfrog.io"
+        organization = "tfstate"
+        token = ${{ secrets.TOKEN }}
+        workspaces {
+          name = "dev"
+            # prefix = "my-prefix-"
+        }
+    }
+}
