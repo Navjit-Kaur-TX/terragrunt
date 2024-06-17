2babbda2350e472bcbaccd3d6356281d832000bd
Navjit-Kaur-TX <132881216+Navjit-Kaur-TX@users.noreply.github.com>
2024-06-17 18:58:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,5 @@
 access_key = ${{ secrets.ACCESS_KEY }}
 secret_key = ${{ secrets.SECRET }}
-#bucket = "aws-tf-state-bucket-tx"  
-#key    = "remotedemo.tfstate"  
-#region = "us-east-1"
-hostname = "gurpreet1.jfrog.io"
-organization = "tf2"
-token = ${{ secrets.TOKEN }}
-workspaces {
-  prefix = "my-prefix-dev"
-}
+bucket = "aws-tf-state-bucket-tx"  
+key    = "remotedemo.tfstate"  
+region = "us-east-1"
